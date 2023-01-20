08181574bcad33c8db4b00f0184cd9002c5c07b2
Ana Trisnanto <trisnanto@gmail.com>
2023-01-20 09:01:44 +0700

    restructure with hapi plugin

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 /* eslint-disable indent */
 /* eslint-disable no-tabs */
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+// const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
 
 const init = async () => {
+  const notesService = new NotesService();
 	const server = Hapi.server({
-		port: 5000,
+		port: 3000,
 		host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
 		routes: {
 			cors: {
@@ -14,7 +17,14 @@ const init = async () => {
 		},
 	});
 
-	server.route(routes);
+	// server.route(routes);
+
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
 
 	await server.start();
 	console.log(`Server berjalan pada ${server.info.uri}`);
